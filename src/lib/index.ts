bc5f1d9520a9f1904a357d61b987ea58c091ab41
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-08-14 19:39:28 +0000

    chore(index): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
-export { DescriptorChainCore } from './descriptor-chain-core.abstract';
 export { DescriptorChain } from './descriptor-chain.class';
+export { DescriptorChainBase } from './descriptor-chain-base.abstract';
+export { DescriptorChainCore } from './descriptor-chain-core.abstract';
